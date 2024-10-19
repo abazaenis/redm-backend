d06a9b630d1c9b49d1ba0c46c0bbda453255a175
Ibrahim Efendić <ibrahim.efendic@skyline.be>
2024-10-19 20:47:56 +0200

    Added JSON property names

--- Dtos/Auth/GoogleSignInVMDto.cs
+++ Dtos/Auth/GoogleSignInVMDto.cs
@@ -1,23 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Redm_backend.Dtos.Auth
 {
     public class GoogleSignInVMDto
     {
         [Required]
+        [JsonPropertyName("id")]
         public string Id { get; set; }
 
         [Required]
         [EmailAddress] // This will validate the email format
+        [JsonPropertyName("email")]
         public string Email { get; set; }
 
         [Required]
+        [JsonPropertyName("given_name")]
         public string GivenName { get; set; }
 
         [Required]
+        [JsonPropertyName("family_name")]
         public string FamilyName { get; set; }
 
         [Required]
+        [JsonPropertyName("name")]
         public string Name { get; set; }
     }
 }
