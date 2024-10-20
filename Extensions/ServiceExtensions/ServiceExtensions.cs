36d8b6b938e873c858d9df2c1341185c073f34c0
abazaenis <enis.abaza@hotmail.com>
2024-10-20 16:29:40 +0200

    Program.cs code cleanup.

---
+++ Extensions/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,112 @@
+﻿namespace Redm_backend.Extensions.ServiceExtensions
+{
+    using System.Text;
+    using Azure.Storage.Blobs;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.OpenApi.Models;
+    using Redm_backend.Data;
+    using Redm_backend.Dtos.PeriodHistory;
+    using Redm_backend.Services.AdminService;
+    using Redm_backend.Services.CronService;
+    using Redm_backend.Services.HomePageService;
+    using Redm_backend.Services.PeriodService;
+    using Redm_backend.Services.PostService;
+    using Redm_backend.Services.ProductService;
+    using Redm_backend.Services.StoryService;
+    using Redm_backend.Services.SymptomService;
+    using Redm_backend.Services.UserService;
+    using Swashbuckle.AspNetCore.Filters;
+
+    public static class ServiceExtensions
+	{
+		public static void ConfigureCors(this IServiceCollection services) => // TODO: restrict CORS only to EXPO requests
+			services.AddCors(options =>
+			{
+				options.AddPolicy("AllowAll", policy =>
+				{
+					policy.AllowAnyOrigin()
+						  .AllowAnyHeader()
+						  .AllowAnyMethod();
+				});
+			});
+
+		public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContext<DataContext>(options => options.UseNpgsql(connectionString));
+		}
+
+		public static void AddBlobServiceClient(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddSingleton(_ => new BlobServiceClient(configuration.GetConnectionString("AzureStorage")));
+		}
+
+		public static void AddCustomServices(this IServiceCollection services)
+		{
+			services.AddScoped<IAuthRepository, AuthRepository>();
+			services.AddScoped<ISymptomService, SymptomService>();
+			services.AddScoped<IUserService, UserService>();
+			services.AddScoped<IPostCategoryService, PostCategoryService>();
+			services.AddScoped<IPostService, PostService>();
+			services.AddScoped<IStoryService, StoryService>();
+			services.AddScoped<IPeriodHistoryService, PeriodHistoryService>();
+			services.AddScoped<IHomePageService, HomePageService>();
+			services.AddScoped<IProductService, ProductService>();
+			services.AddScoped<ICronService, CronService>();
+			services.AddAutoMapper(typeof(Program).Assembly);
+			services.AddHttpContextAccessor();
+		}
+
+		public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+		{
+			var key = Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value!);
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(key),
+						ValidateIssuer = false,
+						ValidateAudience = false,
+						ValidateLifetime = true,
+						ClockSkew = TimeSpan.Zero,
+					};
+				});
+		}
+
+		public static void AddAuthorizationPolicies(this IServiceCollection services)
+		{
+			services.AddAuthorizationBuilder()
+				.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+		}
+
+		public static void ConfigureJsonOptions(this IServiceCollection services)
+		{
+			services.AddControllers().AddJsonOptions(options =>
+			{
+				options.JsonSerializerOptions.Converters.Add(new CalendarColorConverter());
+				options.JsonSerializerOptions.Converters.Add(new ActionConverter());
+			});
+		}
+
+		public static void ConfigureSwagger(this IServiceCollection services)
+		{
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen(c =>
+			{
+				c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+				{
+					Description = """Standard Authorization header using the Bearer scheme. Example: "bearer {token}""",
+					In = ParameterLocation.Header,
+					Name = "Authorization",
+					Type = SecuritySchemeType.ApiKey,
+				});
+				c.OperationFilter<SecurityRequirementsOperationFilter>();
+				c.EnableAnnotations();
+			});
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,8 @@
 namespace Redm_backend
 {
-	using Azure.Storage.Blobs;
-
-	using Microsoft.AspNetCore.Authentication.JwtBearer;
-	using Microsoft.EntityFrameworkCore;
-	using Microsoft.IdentityModel.Tokens;
-	using Microsoft.OpenApi.Models;
-
-	using Redm_backend.Data;
-	using Redm_backend.Dtos.PeriodHistory;
-	using Redm_backend.Services.AdminService;
-	using Redm_backend.Services.CronService;
-	using Redm_backend.Services.HomePageService;
-	using Redm_backend.Services.PeriodService;
-	using Redm_backend.Services.PostService;
-	using Redm_backend.Services.ProductService;
-	using Redm_backend.Services.StoryService;
-	using Redm_backend.Services.SymptomService;
-	using Redm_backend.Services.UserService;
-
-	using Swashbuckle.AspNetCore.Filters;
+	using Microsoft.Extensions.Azure;
+	using Microsoft.Extensions.DependencyInjection;
+	using Redm_backend.Extensions.ServiceExtensions;
 
 	public static class Program
 	{
@@ -27,83 +10,24 @@ namespace Redm_backend
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Database Connection
-			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-			builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connectionString));
-
-			builder.Services.AddSingleton(x => new BlobServiceClient(builder.Configuration.GetConnectionString("AzureStorage")));
-
-			// Add services to the container.
-			builder.Services.AddControllers()
-				.AddJsonOptions(options =>
-				{
-					options.JsonSerializerOptions.Converters.Add(new CalendarColorConverter());
-					options.JsonSerializerOptions.Converters.Add(new ActionConverter());
-				});
-
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(c =>
-			{
-				c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-				{
-					Description = """Standard Authorization header using the Bearer scheme. Example: "bearer {token}""",
-					In = ParameterLocation.Header,
-					Name = "Authorization",
-					Type = SecuritySchemeType.ApiKey,
-				});
-				c.OperationFilter<SecurityRequirementsOperationFilter>();
-				c.EnableAnnotations();
-			});
-			builder.Services.AddAutoMapper(typeof(Program).Assembly);
-			builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-			builder.Services.AddScoped<ISymptomService, SymptomService>();
-			builder.Services.AddScoped<IUserService, UserService>();
-			builder.Services.AddScoped<IPostCategoryService, PostCategoryService>();
-			builder.Services.AddScoped<IPostService, PostService>();
-			builder.Services.AddScoped<IStoryService, StoryService>();
-			builder.Services.AddScoped<IPeriodHistoryService, PeriodHistoryService>();
-			builder.Services.AddScoped<IHomePageService, HomePageService>();
-			builder.Services.AddScoped<IProductService, ProductService>();
-			builder.Services.AddScoped<ICronService, CronService>();
-			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuerSigningKey = true,
-						IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
-						ValidateIssuer = false,
-						ValidateAudience = false,
-						ValidateLifetime = true,
-						ClockSkew = TimeSpan.Zero,
-					};
-				});
-			builder.Services.AddAuthorization(options =>
-			{
-				options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
-			});
-			builder.Services.AddHttpContextAccessor();
-
-			builder.Services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll", policy =>
-				{
-					policy.AllowAnyOrigin()
-						  .AllowAnyHeader()
-						  .AllowAnyMethod();
-				});
-			});
+			// Configure Services
+			builder.Services.AddDatabase(builder.Configuration);
+			builder.Services.AddBlobServiceClient(builder.Configuration);
+			builder.Services.AddCustomServices();
+			builder.Services.ConfigureJsonOptions();
+			builder.Services.AddJwtAuthentication(builder.Configuration);
+			builder.Services.AddAuthorizationPolicies();
+			builder.Services.ConfigureSwagger();
+			builder.Services.ConfigureCors();
 
 			var app = builder.Build();
 
+			// Configure Middleware Pipeline
 			app.UseSwagger();
 			app.UseSwaggerUI();
-
 			app.UseCors("AllowAll");
-
+			app.UseAuthentication();
 			app.UseAuthorization();
-
 			app.MapControllers();
 
 			app.Run();
