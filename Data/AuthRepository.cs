204c600600a6ffc05d810b223ee8301a7436e1cc
abazaenis <enis.abaza@hotmail.com>
2024-11-05 19:38:56 +0100

    Testing refresh token

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -295,7 +295,7 @@
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddHours(5),
+                Expires = DateTime.UtcNow.AddSeconds(5),
                 SigningCredentials = creds,
             };
 
